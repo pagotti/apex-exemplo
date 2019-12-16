98c1cca8c9e6b24436a783621f7967ee75b4207b
pagotti <pagotti@gmail.com>
2019-12-15 21:17:55 -0300

    homework ENCOTEL

---
+++ Encotel/Encotel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Encotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Fazer um tradutor de letras para números de telefone
+            /// Se for 0, 1 ou -, não traduz, caso contário use a tabela abaixo
+            /// 
+            /// Letras	Número
+            /// ABC     2
+            /// DEF     3
+            /// GHI     4
+            /// JKL     5
+            /// MNO     6
+            /// PQRS    7
+            /// TUV     8
+            /// WXYZ    9
+         
+            Console.WriteLine("Entre com o número codificado do telefone:");
+            string numero = Console.ReadLine();
+
+            Telefone tel = new Telefone();
+            Console.WriteLine("Número Real: {0}", tel.Traduzir(numero));
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Encotel/Encotel/Telefone.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encotel
+{
+    class Telefone
+    {
+        private string[] mapa;
+
+        public Telefone()
+        {
+            this.mapa = new string[10];
+            this.mapa[0] = "0";
+            this.mapa[1] = "1";
+            this.mapa[2] = "ABC";
+            this.mapa[3] = "DEF";
+            this.mapa[4] = "GHI";
+            this.mapa[5] = "JKL";
+            this.mapa[6] = "MNO";
+            this.mapa[7] = "PQRS";
+            this.mapa[8] = "TUV";
+            this.mapa[9] = "WXYZ";
+
+        }
+
+        /// <summary>
+        /// Busca o número que corresponde ao caractere informado usando o array do mapa
+        /// </summary>
+        /// <param name="caractere">número 0, 1, hífen ou letra de A a Z</param>
+        /// <returns></returns>
+        private char TraduzirCaractere(char caractere)
+        {
+            if (caractere == '-')
+            {
+                return caractere;
+            }
+
+            for (int i = 0; i < this.mapa.Length; i++)
+            {
+                if (this.mapa[i].Contains(caractere))
+                {
+                    return Convert.ToString(i)[0];
+                }
+            }
+
+            throw new ArgumentException("Caractere inválido", "caractere");
+        }
+
+        public string Traduzir(string letras)
+        {
+            string resultado = "";
+
+            // usa o ToUpper para o caso do usuário fornecer
+            // o telefone com letras minúsculas
+            foreach(char caractere in letras.ToUpper())
+            {
+                resultado += TraduzirCaractere(caractere);
+            }
+
+            return resultado;
+        }
+    }
+}
