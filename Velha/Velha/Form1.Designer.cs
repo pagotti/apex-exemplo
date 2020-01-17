d231e4039d32ac24478f242dd58002e59c164aae
pagotti <pagotti@gmail.com>
2020-01-17 19:41:11 -0300

    Jogo da Velha

---
+++ Velha/Velha/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.optX = new System.Windows.Forms.RadioButton();
+            this.optO = new System.Windows.Forms.RadioButton();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(166, 55);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(42, 45);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 55);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(42, 45);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(213, 107);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(42, 45);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(166, 107);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(42, 45);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(118, 107);
+            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(42, 45);
+            this.button7.TabIndex = 5;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(213, 158);
+            this.button8.Margin = new System.Windows.Forms.Padding(4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(42, 45);
+            this.button8.TabIndex = 10;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(166, 158);
+            this.button9.Margin = new System.Windows.Forms.Padding(4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(42, 45);
+            this.button9.TabIndex = 9;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(118, 158);
+            this.button10.Margin = new System.Windows.Forms.Padding(4);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(42, 45);
+            this.button10.TabIndex = 8;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Velha_Click);
+            // 
+            // optX
+            // 
+            this.optX.AutoSize = true;
+            this.optX.Checked = true;
+            this.optX.Location = new System.Drawing.Point(118, 247);
+            this.optX.Margin = new System.Windows.Forms.Padding(4);
+            this.optX.Name = "optX";
+            this.optX.Size = new System.Drawing.Size(116, 22);
+            this.optX.TabIndex = 11;
+            this.optX.TabStop = true;
+            this.optX.Text = "Jogador 1 (X)";
+            this.optX.UseVisualStyleBackColor = true;
+            // 
+            // optO
+            // 
+            this.optO.AutoSize = true;
+            this.optO.Location = new System.Drawing.Point(118, 293);
+            this.optO.Margin = new System.Windows.Forms.Padding(4);
+            this.optO.Name = "optO";
+            this.optO.Size = new System.Drawing.Size(118, 22);
+            this.optO.TabIndex = 12;
+            this.optO.Text = "Jogador 2 (O)";
+            this.optO.UseVisualStyleBackColor = true;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(118, 343);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(137, 28);
+            this.btnNovo.TabIndex = 13;
+            this.btnNovo.Text = "Novo Jogo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 383);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.optO);
+            this.Controls.Add(this.optX);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.RadioButton optX;
+        private System.Windows.Forms.RadioButton optO;
+        private System.Windows.Forms.Button btnNovo;
+    }
+}
+
---
+++ Velha/Velha/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Velha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Velha_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text.Length == 0)
+            {
+                if (optX.Checked)
+                {
+                    ((Button)sender).Text = "X";
+                    optO.Checked = true;
+                }
+                else
+                {
+                    ((Button)sender).Text = "O";
+                    optX.Checked = true;
+                }
+            }
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            optX.Checked = true;
+
+            foreach (Control ctr in Controls)
+            {
+                if (ctr is Button && ctr.Name != "btnNovo")
+                {
+                    ctr.Text = "";
+                }
+            }
+        }
+    }
+}
